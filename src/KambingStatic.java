20fb77ae9baeee4d16338496b8c62eb1e22d45d4
ACER <ACER@DESKTOP-JMQ2UKH>
2020-10-17 17:27:06 +0700

    PBOIF2-10119076-Latihan6-Kambing Static & Konstanta

---
+++ src/KambingStatic.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author ACER
+ * NAMA     : Sulthon Naufal Akmal
+ * KELAS    : IF2
+ * NIM      : 10119076
+ * Deskripsi Program : Kambing Static
+ */
+public class KambingStatic {
+    //  NAMA_KAMBING sebagai konstanta 
+    public static final  String NAMA_KAMBING = "MIDUN";
+    
+    public static void main(String[] args) {
+        Mamalia.jumlahKambing = 485000;
+        System.out.println(NAMA_KAMBING + "memiliki kambing sebanyak " + Mamalia .jumlahKambing);
+    }
+    
+}
---
+++ src/Mamalia.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author ACER
+ * NAMA     : Sulthon Naufal Akmal
+ * KELAS    : IF2
+ * NIM      : 10119076
+ * Deskripsi Program : mamalia
+ */
+public class Mamalia {
+        //variabel jumlahKambing dideklarasikan sebagai statik
+        public static int jumlahKambing;
+}
